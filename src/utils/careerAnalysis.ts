490f295441d9b350d60225e2818a79785b10db32
MIJINYAWA664 <mijinyawadau664@gmail.com>
2025-09-19 12:41:01 +0100

    Updated careerAnalysis.ts

--- src/utils/careerAnalysis.ts
+++ src/utils/careerAnalysis.ts
@@ -1,1 +1,7 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+// src/utils/careerAnalysis.ts
+export const careerAnalysisError = {
+  code: "rate-limited",
+  message: "You have hit the rate limit. Please upgrade to keep chatting.",
+  providerLimitHit: false,
+  isRetryable: true,
+};
